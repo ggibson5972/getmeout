dba1d61798f3609e7f09acc3512e65d27c677e54
ggibson5972 <grace.gibson5972@gmail.com>
2023-03-01 15:56:52 -0500

    Default zipcode fetched, no testing yet

--- experience/views.py
+++ experience/views.py
@@ -1,8 +1,13 @@
+import json
+
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+import os
+from urllib.request import urlopen
+import re as r
 from .google_search import google
 
 
@@ -11,16 +16,23 @@ CSE_ID = "361ef2027456b4ed6"
 
 
 #  events
-
-
 class EventList(APIView):
 
     def get(self, request):
-        zipcode = request.GET.get('zipcode', '')
-        assert len(zipcode) == 5, "Invalid Zipcode"
+        zipcode = _get_estimated_location()
+
+        query_param: str = request.GET.get('zipcode', zipcode)
+        if query_param:
+            zipcode = query_param
+        assert len(str(zipcode)) == 5, f'Invalid Zipcode: {zipcode}'
         payload = google(f'salsa dancing events {zipcode}', API_KEY, CSE_ID)
         return Response(payload)
 
 
-# def events_view(request):
-#     location = - request.GET['zipcode']
+# Helper Methods
+
+def _get_estimated_location():
+    url = 'http://ipinfo.io/json'
+    response = urlopen(url)
+    data = json.load(response)
+    return str(data['postal'])
