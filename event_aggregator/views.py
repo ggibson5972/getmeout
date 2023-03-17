6105cec12a165db9a947a05c4ce4dcb118f1167d
ggibson5972 <grace.gibson5972@gmail.com>
2023-03-17 12:34:46 -0400

    Started this, yelp results return as expected

--- event_aggregator/views.py
+++ event_aggregator/views.py
@@ -2,18 +2,31 @@ import json
 
 from decouple import config
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from urllib.request import urlopen
+import requests
+from yelpapi import YelpAPI
 
 from event_aggregator.google import search_engine
 
 
 API_KEY = config('API_KEY')
 CSE_ID = config('CSE_ID')
+EVENT_SEARCH_API_URL = config('YELP_EVENTS_API_URL')
+YELP_API_KEY = config('YELP_API_KEY')
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def yelp_results(request):
+    yelp_results = []
+    with YelpAPI(YELP_API_KEY, timeout_s=3.0) as yelp_api:
+        yelp_results = yelp_api.search_query(categories='dancestudio', location=zipcode)
+    return render(request, 'yelp_results.html', context={'yelp_results': yelp_results})
 
 
 #  events
@@ -29,9 +42,10 @@ class EventList(APIView):
 
         # only get first 10 results
         search_results: list = search_engine(f'salsa dancing events near {zipcode}', API_KEY, CSE_ID)[:10]
-        # for image, ['pagemap']['cse_thumbnail'][0]['src']
-        # return Response(search_results)
+        # return Response(yelp_results)
         return render(request, 'events_list.html', context={'search_results': search_results})
+
+
 
 
 # Helper Methods
